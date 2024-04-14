29c51c472df20e5a6c06a87800844b20db5cbea8
Fikri Nurhakim <108182945+akufikri@users.noreply.github.com>
2024-04-14 11:13:38 +0700

    fixed error

--- src/components/SubMenu.tsx
+++ src/components/SubMenu.tsx
@@ -1,4 +1,13 @@
-export function SubMenu({ number, title, year, title2 }) {
+import React from 'react';
+
+interface SubMenuProps {
+      number: string; // Assuming 'number' is a string based on its usage in JSX
+      title: string;
+      year: string;
+      title2: string;
+}
+
+export function SubMenu({ number, title, year, title2 }: SubMenuProps) {
       return (
             <>
                   <div className="flex justify-between my-36">
@@ -24,5 +33,5 @@ export function SubMenu({ number, title, year, title2 }) {
                         </div>
                   </div>
             </>
-      )
+      );
 }
